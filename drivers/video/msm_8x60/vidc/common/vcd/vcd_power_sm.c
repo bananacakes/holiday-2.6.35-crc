03658883cc4631d6656f37789a0d7b3494c98188
bananacakes <tahitiisland03@gmail.com>
2011-11-23 08:51:35 -0300

    vidc: Fix performance level computation

--- drivers/video/msm_8x60/vidc/common/vcd/vcd_power_sm.c
+++ drivers/video/msm_8x60/vidc/common/vcd/vcd_power_sm.c
@@ -310,7 +310,7 @@ u32 vcd_update_clnt_perf_lvl(
 	struct vcd_dev_ctxt *dev_ctxt = cctxt->dev_ctxt;
 	u32 new_perf_lvl;
 	new_perf_lvl =
-	    frm_p_units * fps->fps_numerator / fps->fps_denominator;
+	    frm_p_units * (fps->fps_numerator / fps->fps_denominator);
 	if (cctxt->status.req_perf_lvl) {
 		dev_ctxt->reqd_perf_lvl =
 		    dev_ctxt->reqd_perf_lvl - cctxt->reqd_perf_lvl +
