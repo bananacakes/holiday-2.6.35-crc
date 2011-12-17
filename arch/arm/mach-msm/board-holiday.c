3534e50561e21022c80b2acd05b376a8d05f2b89
bananacakes <tahitiisland03@gmail.com>
2011-12-17 06:27:47 -0300

    button backlights 20mAh

--- arch/arm/mach-msm/board-holiday.c
+++ arch/arm/mach-msm/board-holiday.c
@@ -2687,7 +2687,7 @@ static struct pm8058_led_config pm_led_config[] = {
 		.duites_size = 8,
 		.duty_time_ms = 32,
 		.lut_flag = PM_PWM_LUT_RAMP_UP | PM_PWM_LUT_PAUSE_HI_EN,
-		.out_current = 40,
+		.out_current = 20,
 	},
 
 };
