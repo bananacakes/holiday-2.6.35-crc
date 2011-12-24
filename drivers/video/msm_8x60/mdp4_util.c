028ca27dc1a3be56c4157c83057d2263d53a4146
bananacakes <tahitiisland03@gmail.com>
2011-12-24 12:17:36 -0300

    msm_fb display increase mdp max burst size

--- drivers/video/msm_8x60/mdp4_util.c
+++ drivers/video/msm_8x60/mdp4_util.c
@@ -216,7 +216,7 @@ void mdp4_fetch_cfg(uint32 core_clk)
 	/* MDP cmd block enable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 
-	if (core_clk >= 90000000) { /* 90 Mhz */
+	if (mdp_rev >= MDP_REV_41 || core_clk >= 90000000) { /* 90 Mhz */
 		dmap_data = 0x47; /* 16 bytes-burst x 8 req */
 		vg_data = 0x47; /* 16 bytes-burs x 8 req */
 	} else {
