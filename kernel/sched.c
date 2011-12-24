4dc75a10af3444a0ed4bce242639ba2b561bd37e
bananacakes <tahitiisland03@gmail.com>
2011-12-24 12:18:38 -0300

    Set the command name of the idle tasks in SMP kernels v3

--- kernel/sched.c
+++ kernel/sched.c
@@ -5560,6 +5560,9 @@ void __cpuinit init_idle(struct task_struct *idle, int cpu)
 	 */
 	idle->sched_class = &idle_sched_class;
 	ftrace_graph_init_task(idle);
+#if defined(CONFIG_SMP)
+	sprintf(idle->comm, "%s/%d", INIT_TASK_COMM, cpu);
+#endif	
 }
 
 /*
