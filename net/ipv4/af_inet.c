a5593b41b7a7119cc9b102dd84e62f13c3c4d5a9
bananacakes <tahitiisland03@gmail.com>
2012-03-03 13:01:56 -0300

    instead of nuking clockd (snq-)

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -481,8 +481,8 @@ int inet_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
 	if (addr_len < sizeof(struct sockaddr_in))
 		goto out;
 
-	if (addr->sin_family != AF_INET)
-		goto out;
+	//if (addr->sin_family != AF_INET)
+		//goto out;
 
 	chk_addr_ret = inet_addr_type(sock_net(sk), addr->sin_addr.s_addr);
 
