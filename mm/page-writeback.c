faa6478d7c5e1768ecbdb069e31517116eedd60c
bananacakes <tahitiisland03@gmail.com>
2012-03-03 13:01:37 -0300

    mm Tweak Pagewriteback

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -61,7 +61,7 @@ static inline long sync_writeback_pages(unsigned long dirtied)
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 60;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -78,7 +78,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-int vm_dirty_ratio = 10;
+int vm_dirty_ratio = 5;
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
@@ -94,7 +94,7 @@ unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
 /*
  * The longest time for which data is allowed to remain dirty
  */
-unsigned int dirty_expire_interval = 30 * 100; /* centiseconds */
+unsigned int dirty_expire_interval = 10 * 100; /* centiseconds */
 
 /*
  * The maximum number of pages to write out in a single bdflush/kupdate
