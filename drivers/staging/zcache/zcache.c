5c32e8279ae7d2b707b10f5f24f0b133b4e1bc2e
bananacakes <tahitiisland03@gmail.com>
2012-03-03 13:08:32 -0300

    mm implement WasActive page flag (for improving cleancache) -backported to 2.6.35 by faux123

--- drivers/staging/zcache/zcache.c
+++ drivers/staging/zcache/zcache.c
@@ -1369,6 +1369,8 @@ static void zcache_cleancache_put_page(int pool_id,
 	u32 ind = (u32) index;
 	struct tmem_oid oid = *(struct tmem_oid *)&key;
 
+	if (!PageWasActive(page))
+		return;
 	if (likely(ind == index))
 		(void)zcache_put_page(pool_id, &oid, index, page);
 }
@@ -1383,6 +1385,8 @@ static int zcache_cleancache_get_page(int pool_id,
 
 	if (likely(ind == index))
 		ret = zcache_get_page(pool_id, &oid, index, page);
+	if (ret == 0)
+		SetPageWasActive(page);
 	return ret;
 }
 
